a8d36d5993ddf64a03962b895f83cbc44465f1b0
Mykhailo Gaidai <mykhailo.gaidai@gmail.com>
2022-12-16 17:49:22 +0200

    Day 12, part 2

--- bin/12/main.dart
+++ bin/12/main.dart
@@ -2,7 +2,56 @@ import 'dart:io';
 
 void main() {
   // part1('bin/12/test');
-  part1('bin/12/input');
+  // part1('bin/12/input');
+  part2('bin/12/test');
+  part2('bin/12/input');
+}
+
+void part2(String file) {
+  // read file as strings
+  final input = File(file).readAsLinesSync();
+
+  final heightMap = <String, int>{};
+  final stepsMap = <String, int>{};
+
+  var list = <String>[];
+
+  for (var row = 0; row < input.length; row++) {
+    for (var col = 0; col < input[row].length; col++) {
+      var currentHeight = heights.indexOf(input[row][col]);
+      heightMap['$row,$col'] = currentHeight;
+      stepsMap['$row,$col'] = currentHeight < 2 ? 0 : -1;
+      if (currentHeight == 1) {
+        if (!list.contains('$row,$col')) list.add('$row,$col');
+      }
+    }
+  }
+
+  var step = 0;
+  while (list.isNotEmpty) {
+    for (var e in list) {
+      stepsMap[e] = step;
+    }
+    step++;
+
+    list = list
+        .expand((cur) {
+          final row = int.parse(cur.split(',')[0]);
+          final col = int.parse(cur.split(',')[1]);
+          return [
+            '$row,${col - 1}',
+            '$row,${col + 1}',
+            '${row - 1},$col',
+            '${row + 1},$col',
+          ].where((element) => (heightMap[element] ?? heights.length) <= heightMap[cur]! + 1);
+        })
+        .toSet()
+        .where((e) => stepsMap[e] == -1)
+        .toList();
+  }
+
+  var eKey = heightMap.entries.where((element) => element.value == heights.length - 1).first.key;
+  print('part2, $file, ${stepsMap[eKey]}');
 }
 
 void part1(String file) {
@@ -45,7 +94,7 @@ void part1(String file) {
     queue.addAll(cellsAround
         .where((e) => stepsMap[e[0]][e[1]] < 0 && heights.indexOf(heightMap[e[0]][e[1]]) <= currentHeight + 1)
         .map((e) => '${e[0]},${e[1]}')
-        .where((element) => !queue.contains(element)) );
+        .where((element) => !queue.contains(element)));
   }
 
   print('part1,$file , no path found');
