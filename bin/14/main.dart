cbf79301e6b2065d3ed481403e08b39e5d88b66e
Mykhailo Gaidai <mykhailo.gaidai@gmail.com>
2022-12-18 23:00:12 +0200

    Day 14, part 2

--- bin/14/main.dart
+++ bin/14/main.dart
@@ -2,8 +2,71 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  part1('bin/14/test');
-  part1('bin/14/input');
+  // part1('bin/14/test');
+  // part1('bin/14/input');
+  part2('bin/14/test');
+  part2('bin/14/input');
+}
+
+void part2(String file) {
+  final input = File(file).readAsLinesSync();
+
+  var xmin = 500, xmax = 500, ymax = 0;
+
+  var rocks = <String>[];
+  for (final line in input) {
+    var points = line.split(' -> ');
+    // sliding window
+    for (var i = 0; i < points.length - 1; i++) {
+      final start = points[i];
+      final finish = points[i + 1];
+
+      final x1 = int.parse(start.split(',')[0]);
+      final y1 = int.parse(start.split(',')[1]);
+      final x2 = int.parse(finish.split(',')[0]);
+      final y2 = int.parse(finish.split(',')[1]);
+
+      if (x1 == x2) {
+        if (x1 < xmin) xmin = x1;
+        if (x1 > xmax) xmax = x1;
+        for (var y = min(y1, y2); y <= max(y1, y2); y++) {
+          if (y > ymax) ymax = y;
+          rocks.add('$x1,$y');
+        }
+      } else {
+        if (y1 > ymax) ymax = y1;
+        for (var x = min(x1, x2); x <= max(x1, x2); x++) {
+          if (x < xmin) xmin = x;
+          if (x > xmax) xmax = x;
+          rocks.add('$x,$y1');
+        }
+      }
+    }
+  }
+
+  var count = 1;
+  var peaks = <String>['500,0'];
+  while (peaks.isNotEmpty) {
+    final current = peaks.removeAt(0);
+    final x = int.parse(current.split(',')[0]);
+    final y = int.parse(current.split(',')[1]);
+    if (!rocks.contains('${x - 1},${y + 1}') && y < ymax + 1) {
+      peaks.add('${x - 1},${y + 1}');
+      rocks.add('${x - 1},${y + 1}');
+      count++;
+    }
+    if (!rocks.contains('$x,${y + 1}') && y < ymax + 1) {
+      peaks.add('$x,${y + 1}');
+      rocks.add('$x,${y + 1}');
+      count++;
+    }
+    if (!rocks.contains('${x + 1},${y + 1}') && y < ymax + 1) {
+      peaks.add('${x + 1},${y + 1}');
+      rocks.add('${x + 1},${y + 1}');
+      count++;
+    }
+  }
+  print('Part 2: $file, $count');
 }
 
 void part1(String file) {
