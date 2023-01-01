a155bc1f1182e8f4070228b629921e49a65eaf2f
Mykhailo Gaidai <mykhailo.gaidai@gmail.com>
2023-01-01 18:15:32 +0200

    Day 21 part 2

--- bin/21/main.dart
+++ bin/21/main.dart
@@ -1,8 +1,86 @@
 import 'dart:io';
 
+
 void main() {
   assert(solve1('bin/21/test') == 152);
   assert(solve1('bin/21/input') == 84244467642604);
+  assert(solve2('bin/21/test') == 301);
+  print(solve2('bin/21/input'));
+}
+
+int solve2(String filename) {
+  var rawInput = File(filename).readAsLinesSync();
+  var input = <String, String>{};
+  for (var value in rawInput) {
+    var parts = value.split(': ');
+    input[parts[0]] = parts[1];
+  }
+
+  bool containsHumn(String what) {
+    if (what == 'humn') {
+      return true;
+    }
+    var valueOfWhat = input[what];
+    if (valueOfWhat == null) {
+      print('no value for $what');
+      return false;
+    }
+    var parts = valueOfWhat.split(' ');
+    if (parts.length == 1) {
+      return false;
+    }
+    return [parts[0], parts[2]].any((element) => containsHumn(element));
+  }
+
+  var rootParts = input['root']!.split(' ');
+  var contains = containsHumn(rootParts[0]) ? rootParts[0] : rootParts[2];
+  var targetName = contains == rootParts[0] ? rootParts[2] : rootParts[0];
+
+  int calculate(String name) {
+    var split = input[name]!.split(' ');
+    if (split.length == 1) {
+      return int.parse(split[0]);
+    }
+
+    switch(split[1]) {
+      case '+': return calculate(split[0]) + calculate(split[2]);
+      case '*': return calculate(split[0]) * calculate(split[2]);
+      case '-': return calculate(split[0]) - calculate(split[2]);
+      default: return calculate(split[0]) ~/ calculate(split[2]);
+    }
+  }
+
+  var calculate2 = calculate(targetName);
+
+  int findHumn(String name, int target) {
+    var split = input[name]!.split(' ');
+    if (name == 'humn') {
+      return target;
+    }
+
+    var containsH = containsHumn(split[0]) ? split[0] : split[2];
+    var another = containsH == split[0] ? split[2] : split[0];
+
+    switch(split[1]) {
+      case '+': return findHumn(containsH, target - calculate(another));
+      case '*': return findHumn(containsH, target ~/ calculate(another));
+      case '-':
+        if (containsH == split[0]) {
+          return findHumn(containsH, target + calculate(another));
+        } else {
+          return findHumn(containsH, calculate(another) - target);
+        }
+      default:
+        if (containsH == split[0]) {
+          return findHumn(containsH, target * calculate(another));
+        } else {
+          return findHumn(containsH, calculate(another) ~/ target);
+        }
+    }
+  }
+
+  var result = findHumn(contains, calculate2);
+  return result;
 }
 
 int solve1(String filename) {
